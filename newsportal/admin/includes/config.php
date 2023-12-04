2f0c3604e2f32028e9fb82a23f7fa627b13ef66c
Bharathkanapareddy12 <136273934+Bharathkanapareddy12@users.noreply.github.com>
2023-12-05 00:51:20 +0530

    Update config.php

--- newsportal/admin/includes/config.php
+++ newsportal/admin/includes/config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER','sqlserver189.mysql.database.azure.com');
-define('DB_USER','bhrath');
+define('DB_USER','bharath');
 define('DB_PASS' ,'Password@1');
 define('DB_NAME','newsportal');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
